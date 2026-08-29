9e8a92f083f1ee69767a7c858f730221fd575b22
Vedant Bansal <vedantbansal.uk@gmail.com>
2026-08-29 16:00:27 +0100

    Created the File Select and Cancel Buttons

---
+++ __init__.py
@@ -0,0 +1,62 @@
+#Libraries from aqt
+from aqt import mw
+from aqt.utils import showInfo #This is not needed once the take photo feature is made
+from aqt.qt import *
+from aqt.qt import QDialog, QDialogButtonBox, QAction, QLabel, QVBoxLayout, QFrame, QLineEdit
+
+#Path Library for real cd
+from pathlib import Path
+
+#Option Select GUI
+def option_select() -> None:
+    label = QLabel("Select file select type")
+    button_box = QDialogButtonBox()
+    box_layout = QVBoxLayout()
+    file_option = QDialog(mw)
+    line = QFrame()
+    file_option.setModal(True)
+    file_option.setWindowTitle("OCR Card Reader")
+    label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    label.setStyleSheet("font: 20px Fredo; font-weight: bold; padding-top: 20px; padding-bottom: 15px;")
+    box_layout.addWidget(label)
+    line.setFrameShape(QFrame.Shape.HLine)
+    box_layout.addWidget(line)
+    file_select_btn = button_box.addButton("Select file", QDialogButtonBox.ButtonRole.AcceptRole)
+    take_photo_btn = button_box.addButton("Take Photo", QDialogButtonBox.ButtonRole.ActionRole)
+    cancel_btn = button_box.addButton("Cancel", QDialogButtonBox.ButtonRole.RejectRole)
+    button_box.setStyleSheet("display: flex; width: 65px; height: 20px; margin-right: 10px; margin-left: 10px; border: 2px solid darkgrey; margin-top: 15px; margin-bottom: 7.5px; justify-content: spaced-evenly; padding-left: 25px; padding-right: 25px;")
+    box_layout.addWidget(button_box)
+    #box_layout.setContentsMargins(20, 20, 20, 20)
+    file_option.setLayout(box_layout)
+
+    def on_click(btn) -> None:
+        if btn == file_select_btn:
+            open_files = select_files()
+            current_dir = Path(__file__).parent.absolute()
+            image_path = f"{current_dir}/ImagePaths.txt"
+            with open(image_path, "w") as Paths:
+                for image in open_files:
+                    Paths.write(image + "\n")
+            #API Key Input
+        elif btn == take_photo_btn:
+            take_image()
+        elif btn == cancel_btn:
+            cancel()
+
+    # File select functions:
+    def select_files():
+        open_files, _ = QFileDialog.getOpenFileNames(mw, "Select files", '', "Image Files (*.jpeg *.jpg *.jpe *.png *.avif)")
+        return open_files
+
+    def take_image():
+        showInfo("This feature is still being developed! Sorry, please take an image on your phone, upload the files and select the files. Once the image taking feature is implemented in the future then you can use it.")
+
+    def cancel():
+        file_option.close()
+
+    qconnect(button_box.clicked, on_click)
+    file_option.exec()
+
+action = QAction("OCR Card Reader", mw)
+qconnect(action.triggered, option_select)
+mw.form.menuTools.addAction(action)
