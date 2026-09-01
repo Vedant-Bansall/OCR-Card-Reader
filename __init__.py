48bd650fe0b236adc2fbfc148b58af876b6c97ce
Vedant Bansal <vedantbansal.uk@gmail.com>
2026-09-01 14:20:46 +0100

    Creates Flashcard

--- __init__.py
+++ __init__.py
@@ -1,14 +1,12 @@
 #Libraries from aqt
 from aqt import mw
-from aqt.utils import showInfo, showWarning  # This is not needed once the take photo feature is made
+from aqt.utils import showInfo, showCritical  # This is not needed once the take photo feature is made
 from aqt.qt import *
 from aqt.qt import QDialog, QDialogButtonBox, QAction, QLabel, QVBoxLayout, QFrame, QLineEdit
 
 #Path Library for real cd
 import sys
 from pathlib import Path
-
-sys.path.insert(0, str(Path(__file__).parent / "libs"))
 
 from . import double_image_scan
 
@@ -31,7 +29,6 @@ def option_select() -> None:
     cancel_btn = button_box.addButton("Cancel", QDialogButtonBox.ButtonRole.RejectRole)
     button_box.setStyleSheet("display: flex; width: 65px; height: 20px; margin-right: 10px; margin-left: 10px; border: 2px solid darkgrey; margin-top: 15px; margin-bottom: 7.5px; justify-content: spaced-evenly; padding-left: 25px; padding-right: 25px;")
     box_layout.addWidget(button_box)
-    #box_layout.setContentsMargins(20, 20, 20, 20)
     file_option.setLayout(box_layout)
 
     def on_click(btn) -> None:
@@ -45,24 +42,100 @@ def option_select() -> None:
 
             if len(open_files) > 0:
                 if len(open_files) == 2:
+                    #choose provider
+                    config = mw.addonManager.getConfig(__name__)
+
+                    #updates provider
+                    def provider_on_click(prov_btn):
+                        if prov_btn == claude:
+                            config["provider"] = "Claude"
+                            mw.addonManager.writeConfig(__name__, config)
+                            sys.path.insert(0, str(Path(__file__).parent / "libs" / "claude"))
+                            provider_dialog.close()
+                        elif prov_btn == gemini:
+                            config["provider"] = "Gemini"
+                            mw.addonManager.writeConfig(__name__, config)
+                            sys.path.insert(0, str(Path(__file__).parent / "libs" / "google-genai"))
+                            provider_dialog.close()
+                        elif prov_btn == chatGPT:
+                            config["provider"] = "ChatGPT"
+                            mw.addonManager.writeConfig(__name__, config)
+                            sys.path.insert(0, str(Path(__file__).parent / "libs" / "openai"))
+                            provider_dialog.close()
+
+                    provider_label = QLabel("Select AI provider")
+                    provider_button_box = QDialogButtonBox()
+                    provider_box_layout = QVBoxLayout()
+                    provider_dialog = QDialog(mw)
+                    provider_line = QFrame()
+                    provider_dialog.setModal(True)
+                    provider_dialog.setWindowTitle("Select AI provider")
+                    provider_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+                    provider_label.setStyleSheet("font: 20px Fredo; font-weight: bold; padding-top: 20px; padding-bottom: 15px;")
+                    provider_box_layout.addWidget(provider_label)
+                    provider_line.setFrameShape(QFrame.Shape.HLine)
+                    provider_box_layout.addWidget(provider_line)
+                    claude = provider_button_box.addButton("Claude", QDialogButtonBox.ButtonRole.AcceptRole)
+                    gemini = provider_button_box.addButton("Gemini", QDialogButtonBox.ButtonRole.ActionRole)
+                    chatGPT = provider_button_box.addButton("ChatGPT", QDialogButtonBox.ButtonRole.HelpRole)
+                    provider_button_box.setStyleSheet("display: flex; width: 65px; height: 20px; margin-right: 10px; margin-left: 10px; border: 2px solid darkgrey; margin-top: 15px; margin-bottom: 7.5px; justify-content: spaced-evenly; padding-left: 25px; padding-right: 25px;")
+                    provider_box_layout.addWidget(provider_button_box)
+                    provider_dialog.setLayout(provider_box_layout)
+                    qconnect(provider_button_box.clicked, provider_on_click)
+                    provider_dialog.exec()
+
                     #add API key to config.json function
                     def add_api_config():
-                        #debug - showInfo(f"Attempting to save: '{api_line_edit.text()}'")
-                        config = mw.addonManager.getConfig(__name__)
-                        config["api_key"] = api_line_edit.text()
-                        mw.addonManager.writeConfig(__name__, config)
-                        api_dialog.close()
-                        #more debugging - saved = mw.addonManager.getConfig(__name__)
-                        #also this - showInfo(str(saved))
+                        also_config = mw.addonManager.getConfig(__name__)
+                        if also_config["provider"] == "Claude":
+                            prov_len = len(also_config["provider"])
+                            if len(api_line_edit.text()) == 0 and prov_len != 0:
+                                api_dialog.close()
+
+                            else:
+                                config_3 = mw.addonManager.getConfig(__name__)
+                                config_3["api_key"] = api_line_edit.text()
+                                mw.addonManager.writeConfig(__name__, config_3)
+                                api_dialog.close()
+
+                            double_image_scan.start_scan()
+
+                        elif also_config["provider"] == "Gemini":
+                            prov_len = len(also_config["provider"])
+                            if len(api_line_edit.text()) == 0 and prov_len != 0:
+                                api_dialog.close()
+
+                            else:
+                                config_3 = mw.addonManager.getConfig(__name__)
+                                config_3["api_key"] = api_line_edit.text()
+                                mw.addonManager.writeConfig(__name__, config_3)
+                                api_dialog.close()
+
+                            double_image_scan.start_scan()
+
+
+                        elif also_config["provider"] == "ChatGPT":
+                            prov_len = len(also_config["provider"])
+                            if len(api_line_edit.text()) == 0 and prov_len != 0:
+                                api_dialog.close()
+
+                            else:
+                                config_3 = mw.addonManager.getConfig(__name__)
+                                config_3["api_key"] = api_line_edit.text()
+                                mw.addonManager.writeConfig(__name__, config_3)
+                                api_dialog.close()
+
+                            double_image_scan.start_scan()
 
                     #Close File Option
                     file_option.close()
 
                     #API Key Input
-                    api_label = QLabel("Please enter a Google Gemini API Key. Click enter to continue once inputted.")
+                    api_label = QLabel("Please enter an API key (for the respective provider you chose). Click enter to continue once inputted.")
                     api_label.setStyleSheet("font: 20px Fredo; font-weight: bold; padding-top: 20px; padding-bottom: 15px;")
-                    tutorial_label = QLabel(f'To see a tutorial on how to create one, <a href="https://youtu.be/Cl4XKgz6EJQ?si=4MlxM4faNWoTgugH">Click here</a>')
+                    tutorial_label = QLabel(f'To see a tutorial on how to create a claude api key, <a href="https://youtu.be/vgncj7MJbVU?si=uZRm9WaEkbO4xn2X">Click here</a>; To see a tutorial on how to create a gemini api key, <a href="https://youtu.be/Cl4XKgz6EJQ?si=4MlxM4faNWoTgugH">Click here</a>; To see a tutorial on how to create a ChatGPT api key, <a href="https://youtu.be/SzPE_AE0eEo?si=e1edO44gxAK1uV_p">Click here</a>')
                     tutorial_label.setOpenExternalLinks(True)
+                    info_label = QLabel("NOTE: if you have already previously entered an API key from a provider you have used this script on before (e.g you have previously inserted a gemini key), click enter with nothing in the input bar but you must have used that provider the most recent time you have ran this addon.")
                     api_box_layout = QVBoxLayout()
                     api_line_edit = QLineEdit()
                     api_line_edit.setPlaceholderText("Enter API Key")
@@ -73,11 +146,12 @@ def option_select() -> None:
                     api_box_layout.addWidget(api_label)
                     api_box_layout.addWidget(api_line_edit)
                     api_box_layout.addWidget(tutorial_label)
+                    api_box_layout.addWidget(info_label)
                     api_dialog.setLayout(api_box_layout)
                     api_dialog.exec()
                 else:
                     file_option.close()
-                    showWarning("Please insert 2 files (Front and Back respectively)!")
+                    showCritical("Please insert 2 files (Front and Back respectively)!")
             else:
                 file_option.close()
 
@@ -101,5 +175,6 @@ def option_select() -> None:
     file_option.exec()
 
 action = QAction("OCR Card Reader", mw)
+action.setShortcut("Ctrl+Shift+R")
 qconnect(action.triggered, option_select)
 mw.form.menuTools.addAction(action)
--- double_image_scan.py
+++ double_image_scan.py
@@ -1,74 +1,407 @@
 #Imports
-#Imports for testing
-from aqt.qt import QAction, QThread, qconnect, pyqtSignal
-
-#print("path")
-from pathlib import Path
-#print("path")
-from google import genai
-#print("genai")
+#Qt
+from aqt.qt import QAction, QThread, qconnect, pyqtSignal, QDialog, QLabel, QVBoxLayout, QTextEdit, QFrame, Qt, QPushButton, QComboBox
+from aqt.utils import showCritical
 from aqt import mw
 
-#Creating and configuring Background Thread
-class Background(QThread):
-    scan_finished = pyqtSignal(str, str)
+#Get ImagePaths
+from pathlib import Path
 
-    def run(self):
-        # Get API Key
+def start_scan():
+    global background
+    config = mw.addonManager.getConfig(__name__)
+
+    #Claude's code (get it?, GET IT?, HAHAHAHHA I'M SO FUNNY HAHAHAHAH (im sorry))
+    if config["provider"] == "Claude":
+        import anthropic
+        import base64
+        import mimetypes
+
+        class Background(QThread):
+            scan_finished = pyqtSignal(str, str)
+            error_signal = pyqtSignal(str)
+
+            def __init__(self, api_key: str):
+                super().__init__()
+                self.api_key = api_key
+
+            def run(self):
+                # Get API Key
+                api_key = config["claude_api_key"]
+
+                # Create client
+                client = anthropic.Anthropic(api_key=api_key)
+
+                # Front and Back Variables Creation
+                current_dir = Path(__file__).parent.absolute()
+                image_path = f"{current_dir}/ImagePaths.txt"
+
+                with open(image_path, "r") as Paths:
+                    front = Paths.readline().strip()
+                    back = Paths.readline().strip()
+
+                # Front Base64-Encoded Image
+                mime_type, _ = mimetypes.guess_type(front)
+                front_media_type = mime_type
+                with open(front, "rb") as image_file:
+                    front_image_data = base64.standard_b64encode(image_file.read()).decode("utf-8")
+
+                # Back Base64-Encoded Image
+                mime_type, _ = mimetypes.guess_type(back)
+                back_media_type = mime_type
+                with open(back, "rb") as image_file:
+                    back_image_data = base64.standard_b64encode(image_file.read()).decode("utf-8")
+
+                # Create Front
+                try:
+                    front_interaction = client.messages.create(
+                        model="claude-sonnet-5",
+                        max_tokens=2048,
+                        messages=[
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "image",
+                                        "source": {
+                                            "type": "base64",
+                                            "media_type": front_media_type,
+                                            "data": front_image_data,
+                                        },
+                                    },
+                                    {"type": "text", "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                                ],
+                            }
+                        ],
+                    )
+
+
+                    # Create Back
+                    back_interaction = client.messages.create(
+                        model="claude-sonnet-5",
+                        max_tokens=2048,
+                        messages=[
+                            {
+                                "role": "user",
+                                "content": [
+                                    {
+                                        "type": "image",
+                                        "source": {
+                                            "type": "base64",
+                                            "media_type": back_media_type,
+                                            "data": back_image_data,
+                                        },
+                                    },
+                                    {"type": "text", "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                                ],
+                            }
+                        ],
+                    )
+
+                    self.scan_finished.emit(front_interaction.content[0].text, back_interaction.content[0].text)
+
+                except Exception as e:
+                    self.error_signal.emit(str(e))
+
+        api_key = config["claude_api_key"]
+
+    elif config["provider"] == "Gemini":
+        from google import genai
+
+        class Background(QThread):
+            scan_finished = pyqtSignal(str, str)
+            error_signal = pyqtSignal(str)
+
+            def __init__(self, api_key: str):
+                super().__init__()
+                self.api_key = api_key
+
+            def run(self):
+                # Get API Key
+                config = mw.addonManager.getConfig(__name__)
+                api_key = config["gemini_api_key"]
+
+                # Create client
+                client = genai.Client(api_key=api_key)
+
+                # Front and Back Variables Creation
+                current_dir = Path(__file__).parent.absolute()
+                image_path = f"{current_dir}/ImagePaths.txt"
+
+                with open(image_path, "r") as Paths:
+                    front = Paths.readline().strip()
+                    back = Paths.readline().strip()
+
+                #front and back uploaded
+                front_uploaded = client.files.upload(file=front)
+                back_uploaded = client.files.upload(file=back)
+
+                # Create Front
+                try:
+                    front_interaction = client.interactions.create(
+                        model="gemini-3.6-flash",
+                        input=[
+                            {"type": "text",
+                             "text": "Type out what the image says and only the text (Example of what not to do: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                            {
+                                "type": "image",
+                                "uri": front_uploaded.uri,
+                                "mime_type": front_uploaded.mime_type
+                            }
+                        ]
+                    )
+
+                    # Create Back
+                    back_interaction = client.interactions.create(
+                        model="gemini-3.6-flash",
+                        input=[
+                            {"type": "text",
+                             "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                            {
+                                "type": "image",
+                                "uri": back_uploaded.uri,
+                                "mime_type": back_uploaded.mime_type
+                            }
+                        ]
+                    )
+
+                    self.scan_finished.emit(front_interaction.output_text, back_interaction.output_text)
+
+                except Exception as e:
+                    self.error_signal.emit(str(e))
+
+        api_key = config["gemini_api_key"]
+
+    elif config["provider"] == "ChatGPT":
+        from openai import OpenAI
+        import base64
+        import mimetypes
+
+        class Background(QThread):
+            scan_finished = pyqtSignal(str, str)
+            error_signal = pyqtSignal(str)
+
+            def __init__(self, api_key: str):
+                super().__init__()
+                self.api_key = api_key
+
+            def run(self):
+                # Get API Key
+                config = mw.addonManager.getConfig(__name__)
+                api_key = config["OpenAI_api_key"]
+
+                # Create client
+                client = OpenAI(api_key=api_key)
+
+                # Front and Back Variables Creation
+                current_dir = Path(__file__).parent.absolute()
+                image_path = f"{current_dir}/ImagePaths.txt"
+
+                with open(image_path, "r") as Paths:
+                    front = Paths.readline().strip()
+                    back = Paths.readline().strip()
+
+                with open(front, "rb") as image_file:
+                    front_image_data = base64.standard_b64encode(image_file.read()).decode("utf-8")
+                mime_type, _ = mimetypes.guess_type(front)
+                front_media_type = mime_type
+
+                with open(back, "rb") as image_file:
+                    back_image_data = base64.standard_b64encode(image_file.read()).decode("utf-8")
+                mime_type, _ = mimetypes.guess_type(back)
+                back_media_type = mime_type
+
+                # Create Front
+                try:
+                    #create front
+                    front_interaction = client.responses.create(
+                        model="gpt-5.6-mini",
+                        input=[
+                            {
+                                "role": "user",
+                                "content": [
+                                    {"type": "input_text", "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                                    {
+                                        "type": "input_image",
+                                        "image_url": f"data:{front_media_type};base64,{front_image_data}"
+                                    },
+                                ],
+                            }
+                        ],
+                    )
+
+                    #create back
+                    back_interaction = client.responses.create(
+                        model="gpt-5.6-mini",
+                        input=[
+                            {
+                                "role": "user",
+                                "content": [
+                                    {"type": "input_text",
+                                     "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text. Do not structure it with new lines, rather as a paragraph."},
+                                    {
+                                        "type": "input_image",
+                                        "image_url": f"data:{back_media_type};base64,{back_image_data}"
+                                    },
+                                ],
+                            }
+                        ],
+                    )
+
+                    self.scan_finished.emit(front_interaction.output_text, back_interaction.output_text)
+
+                except Exception as e:
+                    self.error_signal.emit(str(e))
+
+        api_key = config["OpenAI_api_key"]
+
+    background = Background(api_key)
+    qconnect(background.scan_finished, scan_handler)
+    qconnect(background.error_signal, show_error)
+    background.start()
+
+def scan_handler(front_text: str, back_text: str) -> None:
+    def open_back_editor():#
+        front_text_edited = front_editor.toPlainText()
         config = mw.addonManager.getConfig(__name__)
-        api_key = config["api_key"]
+        config["card_front"] = front_text_edited
+        front_dialog.close()
+        back_dialog.exec()
 
-        # print("client created")
-        # Create client
-        client = genai.Client(api_key=api_key)
+    def close_back_editor():
+        back_text_edited = back_editor.toPlainText()
+        config = mw.addonManager.getConfig(__name__)
+        config["card_back"] = back_text_edited
+        back_dialog.close()
+        deck_select()
 
-        # print("variables created")
-        # Front and Back Variables Creation
-        current_dir = Path(__file__).parent.absolute()
-        image_path = f"{current_dir}/ImagePaths.txt"
+    #Back Text Editor
+    back_dialog = QDialog(mw)
+    back_dialog.setModal(True)
+    back_dialog.setFixedSize(800, 650)
+    back_dialog.setWindowTitle("Edit Card Back")
 
-        with open(image_path, "r") as Paths:
-            front = Paths.readline().strip()
-            back = Paths.readline().strip()
+    back_box = QVBoxLayout()
 
-        # print("front and back uploaded")
-        front_uploaded = client.files.upload(file=front)
-        back_uploaded = client.files.upload(file=back)
+    back_label = QLabel("Edit Card Back")
+    back_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    back_label.setStyleSheet("font-size: 50px")
 
-        # Create Front
-        front_interaction = client.interactions.create(
-            model="gemini-3.6-flash",
-            input=[
-                {"type": "text",
-                 "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text"},
-                {
-                    "type": "image",
-                    "uri": front_uploaded.uri,
-                    "mime_type": front_uploaded.mime_type
-                }
-            ]
-        )
+    back_line = QFrame()
+    back_line.setFrameShape(QFrame.Shape.HLine)
 
-        # print("front interaction created")
-        # Create Back
-        back_interaction = client.interactions.create(
-            model="gemini-3.6-flash",
-            input=[
-                {"type": "text",
-                 "text": "Type out what the image says and only the text (Nothing like: 'here is what the image says' and then the text"},
-                {
-                    "type": "image",
-                    "uri": back_uploaded.uri,
-                    "mime_type": back_uploaded.mime_type
-                }
-            ]
-        )
+    back_editor = QTextEdit()
+    back_editor.setFixedSize(780, 450)
+    back_editor.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    back_editor.setPlainText(back_text)
 
-        self.scan_finished.emit(front_interaction.output_text, back_interaction.output_text)
+    back_confirm_button = QPushButton("Confirm")
+    back_confirm_button.setStyleSheet("font-size: 20px; font-weight: bold; background-color: blue; color: white; border: 1px solid white; border-radius: 5px;")
+    back_confirm_button.setFixedSize(150, 75)
+    back_confirm_button.clicked.connect(close_back_editor)
 
-background = Background()
-background.start()
+    back_lower_line = QFrame()
+    back_lower_line.setFrameShape(QFrame.Shape.HLine)
 
-action = QAction("Image Scan Test", mw)
-qconnect(action.triggered, background.run)
-mw.form.menuTools.addAction(action)
+    #Adding everything
+    back_box.addWidget(back_label)
+    back_box.addWidget(back_line)
+    back_box.addWidget(back_editor)
+    back_box.addWidget(back_lower_line)
+    back_box.addWidget(back_confirm_button)
+
+    back_dialog.setLayout(back_box)
+
+    #Front Text Editor
+    front_dialog = QDialog(mw)
+    front_dialog.setModal(True)
+    front_dialog.setFixedSize(800, 650)
+    front_dialog.setWindowTitle("Edit Card Front")
+
+    front_box = QVBoxLayout()
+
+    front_label = QLabel("Edit Card Front")
+    front_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    front_label.setStyleSheet("font-size: 50px")
+
+    front_line = QFrame()
+    front_line.setFrameShape(QFrame.Shape.HLine)
+
+    front_editor = QTextEdit()
+    front_editor.setFixedSize(780, 450)
+    front_editor.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    front_editor.setPlainText(front_text)
+
+    front_confirm_button = QPushButton("Confirm")
+    front_confirm_button.setStyleSheet("font-size: 20px; font-weight: bold; background-color: blue; color: white; border: 1px solid white; border-radius: 5px;")
+    front_confirm_button.setFixedSize(150, 75)
+    front_confirm_button.clicked.connect(open_back_editor)
+
+    front_lower_line = QFrame()
+    front_lower_line.setFrameShape(QFrame.Shape.HLine)
+
+    #Adding everything
+    front_box.addWidget(front_label)
+    front_box.addWidget(front_line)
+    front_box.addWidget(front_editor)
+    front_box.addWidget(front_lower_line)
+    front_box.addWidget(front_confirm_button)
+
+    front_dialog.setLayout(front_box)
+    front_dialog.exec()
+
+def show_error(error: str) -> None:
+    showCritical(str(error))
+
+def deck_select():
+    def confirm_deck():
+        selected_deck = deck_select.currentText()
+        config = mw.addonManager.getConfig(__name__)
+        config["selected_deck"] = selected_deck
+        mw.addonManager.writeConfig(__name__, config)
+        deck_dialog.close()
+        card_creation()
+
+    all_decks = mw.col.decks.all_names()
+
+    #Deck select
+    deck_dialog = QDialog(mw)
+    deck_dialog.setModal(True)
+    deck_dialog.setFixedSize(400, 200)
+    deck_dialog.setWindowTitle("Deck Select")
+
+    deck_label = QLabel("Select the deck you\n want to add this card to")
+    deck_label.setAlignment(Qt.AlignmentFlag.AlignCenter)
+    deck_label.setStyleSheet("font-family: 'Courier New', Courier, monospace; font-size: 24px")
+
+    deck_select = QComboBox()
+    deck_select.setFixedSize(300, 30)
+    deck_select.addItems(all_decks)
+
+    deck_confirm = QPushButton("Confirm")
+    deck_confirm.setFixedSize(75, 30)
+    deck_confirm.clicked.connect(confirm_deck)
+
+    deck_box = QVBoxLayout()
+    deck_box.addWidget(deck_label)
+    deck_box.addWidget(deck_select)
+    deck_dialog.setLayout(deck_box)
+    deck_dialog.exec()
+
+def card_creation():
+    config = mw.addonManager.getConfig(__name__)
+    question = config["card_front"]
+    answer = config["card_back"]
+
+    #Create card
+    deck = mw.col.decks.by_name(config["selected_deck"])
+    note_type = mw.col.models.by_name("Basic")
+    flashcard = mw.col.new_note(note_type)
+    flashcard["Front"] = question
+    flashcard["Back"] = answer
+    mw.col.add_note(flashcard, deck.id)
+
+# Testing
+# action = QAction("Image Scan Test", mw)
+# qconnect(action.triggered, start_scan)
+# mw.form.menuTools.addAction(action)
